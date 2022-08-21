774e03376f88b546322104e8140c970f4768cf91
alvaroormeno <alvaro@ormeno.org>
2022-08-21 14:52:25 -0400

    created new navbar component

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 // Importing all components from CONTAINER folder, using and index.js file inside container folder to easily import using destructure.
 import { About, Footer, Header, Skills, Testimonial, Work} from './container'
+import { Navbar } from './components'
 
 function App() {
   return (
---
+++ frontend_react/src/components/index.js
@@ -0,0 +1,3 @@
+// same as index.js from container file
+
+export { default as Navbar} from './Navbar/Navbar'
