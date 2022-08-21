88c01b82841d151eafb4f4e1ac69b3a4475e15d3
alvaroormeno <alvaro@ormeno.org>
2022-08-21 15:30:40 -0400

    navbar.jsx - declared basic jsx, used map to create a link for each navbar element we have

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,10 +5,19 @@ import {images} from '../../constants/'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt="logo" />
       </div>
+      {/* Loop through all of elements we need to have in navbar  */}
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'contact', 'work', 'skills'].map((item => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div/>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        )))}
+      </ul>
     </nav>
   )
 }
