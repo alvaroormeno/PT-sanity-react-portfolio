f2daaa7828662d62f2bf3935b9955389790dcfd9
alvaroormeno <alvaro@ormeno.org>
2022-08-21 17:36:26 -0400

    completed mobile menu using motion for slide animation. desktip and mobile complete

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.scss'
+import { HiMenuAlt4, HiX} from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import {images} from '../../constants/'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -18,6 +23,28 @@ const Navbar = () => {
           </li>
         )))}
       </ul>
+
+      {/* Mobile Menu */}
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {/* if toggle is true... */}
+        {toggle && (
+          // to use motion we use motion. followed by the element we want to use, in this case div.
+          <motion.div
+            whileInView={{ x: [300, 0]}}
+            transition={{duration: 0.85, ease: 'easeOut'}}
+          >
+            <HiX onClick={() => setToggle(false)}/>
+            <ul>
+              {['home', 'about', 'contact', 'work', 'skills'].map((item => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+              )))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
