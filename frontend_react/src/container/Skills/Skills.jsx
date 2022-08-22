1878a3d0e8569d598f36d78656397e50ec19f4d3
alvaroormeno <alvaro@ormeno.org>
2022-08-22 14:15:24 -0400

    skills.jsx - finished implementing basic jsx of component, need to style scss

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -36,7 +36,21 @@ const Skills = () => {
         <motion.div
           className='app__skills-list'
         >
-          {/* {Skills.map} */}
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{opacity: [0, 1]}}
+              transition={{duration: 0.5}}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div className='app__flex' style={{backgroundColor: skill.bgColor }}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className='p-text'>
+                {skill.name}
+              </p>
+            </motion.div>
+          ))}
         </motion.div>
       </div>
     </>
