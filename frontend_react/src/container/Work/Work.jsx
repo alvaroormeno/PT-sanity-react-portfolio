3fb386c9f085caa7e4feeef9512bc055c46aba4c
alvaroormeno <alvaro@ormeno.org>
2022-08-21 23:55:08 -0400

    work.jsx - implemented filter buttons that are mapped and the divs with motion animation to showcase each work, not completed yet

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Work.scss'
+import {AiFillEye, AiFillGithub} from 'react-icons/ai'
+import {motion} from 'framer-motion'
+import {AppWrap} from '../../wrapper'
+import {urlFor, client} from '../../client'
+
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All')
+  const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
+
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <>
+      <h2 className='head-text'>
+        My Creative <span>Portfolio</span> section 
+      </h2>
+
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : '' } `}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5}}
+        className='app__work-portfolio'
+      >
+
+      </motion.div>
+    </>
   )
 }
 
