943c1cd44c85bf31937fdbbbb981f08e0513309b
alvaroormeno <alvaro@ormeno.org>
2022-08-22 00:09:00 -0400

    works.jsx - implemented useEffect to fetch data from sanity and place it on 2 states

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -11,6 +11,19 @@ const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All')
   const [animateCard, setAnimateCard] = useState({y: 0, opacity: 1})
 
+  const [works, setWorks] = useState([])
+  const [filterWork, setFilterWork] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    })
+  }, [])
+
   const handleWorkFilter = (item) => {
 
   }
