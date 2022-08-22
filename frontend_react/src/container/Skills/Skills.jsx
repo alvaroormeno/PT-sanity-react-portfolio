d674fdec5bef4278b42ae100a4b2ff9674ce3ec5
alvaroormeno <alvaro@ormeno.org>
2022-08-22 14:10:22 -0400

    skills.jsx - implemented useeffect to fetch data from sanity and save it in states for experiencees and skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Skills.scss'
+import {motion} from 'framer-motion'
+import {AppWrap} from '../../wrapper'
+import {urlFor, client} from '../../client'
+import ReactToolTip from 'react-tooltip'
 
 const Skills = () => {
+
+  const [experience, setExperience] = useState([])
+  const [skills, setSkills] = useState([])
+  
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+    .then((data) => {
+      setExperience(data);
+    })
+
+    client.fetch(skillsQuery)
+    .then((data) => {
+      setSkills(data);
+    })
+  }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>
+        Skills & Experience
+      </h2>
+
+      <div className='app__skills-container'>
+        <motion.div
+          className='app__skills-list'
+        >
+          {/* {Skills.map} */}
+        </motion.div>
+      </div>
+    </>
   )
 }
 
-export default Skills
+export default Skills 
