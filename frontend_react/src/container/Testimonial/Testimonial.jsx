ef73220430a66676819cf843c159f9c813799091
alvaroormeno <alvaro@ormeno.org>
2022-08-22 16:17:33 -0400

    testimonials.jsx - implemented states and useeffect to fetch and store data from sanity

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
 import './Testimonial.scss'
+import {motion} from 'framer-motion'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import {AppWrap, MotionWrap} from '../../wrapper'
+import {urlFor, client} from '../../client'
+
 
 const Testimonial = () => {
+
+  const [brands, setBrands] = useState([])
+  const [Testimonials, setTestimonials] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const query = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(query)
+    .then((data) => {
+      // console.log(data)
+      setTestimonials(data);
+    })
+
+    client.fetch(brandsQuery)
+    .then((data) => {
+      setBrands(data);
+    })
+  }, [])
+
+
   return (
     <div>Testimonials</div>
   )
