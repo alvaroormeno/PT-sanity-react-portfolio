ce03201ff2bb515110140a45cc7bd43866b09170
alvaroormeno <alvaro@ormeno.org>
2022-08-21 23:29:38 -0400

    created index.js to export wrapper components and exported new social media and navigationdots compinents on index.js file from components folder

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,3 +1,5 @@
 // same as index.js from container file
 
 export { default as Navbar} from './Navbar/Navbar'
+export { default as NavigationDots} from './NavigationDots'
+export { default as SocialMedia} from './SocialMedia'
